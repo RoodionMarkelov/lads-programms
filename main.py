fdfb59c9154ff63be6a38707579a3d5a366aae16
rodio <rodionmarkelov01229@gmail.com>
2023-09-27 17:35:16 +0400

    parsing

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-# url = "https://www.gismeteo.ru/diary/4618/2023/9/"
-#
-# headers = {
-#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.41"
-# }
-#
-# req = requests.get(url, headers=headers)
-# src = req.text
-# #print(src)
-#
-# with open("index.html", "w") as file:
-#     file.write(src)
+url = "https://www.gismeteo.ru/diary/4618/2023/9/"
 
-with open("index.html") as file:
-    src = file.read()
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.41"
+}
+
+req = requests.get(url, headers=headers)
+src = req.text
+
+
+with open("index.html", "w") as file:
+    file.write(src)
+
+# with open("index.html") as file:
+#     src = file.read()
 
 soup = BeautifulSoup(src, "lxml")
 
@@ -50,3 +50,4 @@ with open('dataset.csv', 'w', newline='') as csvfile:
     for item in data_table:
         file_writer = csv.writer(csvfile, delimiter=",", lineterminator="\r")
         file_writer.writerow([item])
+
