d0662b9b96be5dfff955f8ee02caa3a0fbc2473c
rodio <rodionmarkelov01229@gmail.com>
2023-10-30 14:39:55 +0400

    LAB - 2

--- main.py
+++ main.py
@@ -2,57 +2,74 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-for year in range(1997, 2024):
-    for mouths in range(1, 13):
+# for year in range(2023, 2024):
+#     for mouths in range(1, 13):
+#
+#         url = "https://www.gismeteo.ru/diary/4618/" + str(year) + "/" + str(mouths) + "/"
+#
+#         headers = {
+#                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+#                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.41"
+#         }
+#
+#         req = requests.get(url, headers=headers)
+#         src = req.text
+#         try:
+#             with open("cite.html", "w") as file:
+#                 file.write(src)
+#         except Exception:
+#             continue
+#
+#             # with open("index.html") as file:
+#             #     src = file.read()
+#
+#         soup = BeautifulSoup(src, "lxml")
+#         try:
+#             tab = soup.find("table").find("tbody").find_all("tr")
+#         except Exception:
+#             continue
+#
+#         data_from_table = []
+#         for item in tab:
+#             data_from_table_td = item.find_all('td')
+#             data = data_from_table_td[0].text
+#             temp_morning = data_from_table_td[1].text
+#             pres_morning = data_from_table_td[2].text
+#             wind_morning = data_from_table_td[5].text
+#             temp_evening = data_from_table_td[6].text
+#             pres_evening = data_from_table_td[7].text
+#             wind_evening = data_from_table_td[10].text
+#
+#             data_from_table.append(
+#                 {
+#                     "data": str(year) + "-" + str(mouths) + "-" + data,
+#                     "temp_morning": temp_morning,
+#                     "presure_morning": pres_morning,
+#                     "wind_morning": wind_morning,
+#                     "temp_evening": temp_evening,
+#                     "presure_evening": pres_evening,
+#                     "wind_evening": wind_evening
+#                 }
+#             )
+#
+#         with open('dataset.csv', 'a', newline='') as csvfile:
+#             for item in data_from_table:
+#                file_writer = csv.writer(csvfile, delimiter=",", lineterminator="\r")
+#                file_writer.writerow([item["data"], item["temp_morning"], item["presure_morning"], item["wind_morning"], item["temp_evening"], item["presure_evening"], item["wind_evening"]])
 
-        url = "https://www.gismeteo.ru/diary/4618/" + str(year) + "/" + str(mouths) + "/"
+with open('dataset.csv', newline='') as f:
+    fieldnames = ['data', 'temp_morning', 'presure_morning', 'wind_morning', 'temp_evening', 'presure_evening', 'wind']
+    reader = csv.DictReader(f, fieldnames=fieldnames)
+    for row in reader:
+        file_writer = csv.writer(open('dataset-number.csv', 'a', newline=''), lineterminator="\r")
+        file_writer.writerow([row['data']])
+        file_writer = csv.writer(open('dataset-meteodate.csv', 'a', newline=''), lineterminator="\r")
+        file_writer.writerow([row['temp_morning'], row['presure_morning'], row['wind_morning'], row['temp_evening'], row['presure_evening'], row['wind']])
 
-        headers = {
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.41"
-        }
+with open('dataset.csv', newline='') as f:
+    fieldnames = ['data', 'temp_morning', 'presure_morning', 'wind_morning', 'temp_evening', 'presure_evening', 'wind']
+    reader = csv.DictReader(f, fieldnames=fieldnames)
+    for row in reader:
+        file_writer = csv.writer(open('dataset-number.csv', 'a', newline=''), lineterminator="\r")
+        file_writer.writerow([row['data']])
 
-        req = requests.get(url, headers=headers)
-        src = req.text
-        try:
-            with open("cite.html", "w") as file:
-                file.write(src)
-        except Exception:
-            continue
-
-            # with open("index.html") as file:
-            #     src = file.read()
-
-        soup = BeautifulSoup(src, "lxml")
-        try:
-            tab = soup.find("table").find("tbody").find_all("tr")
-        except Exception:
-            continue
-
-        data_from_table = []
-        for item in tab:
-            data_from_table_td = item.find_all('td')
-            data = data_from_table_td[0].text
-            temp_morning = data_from_table_td[1].text
-            pres_morning = data_from_table_td[2].text
-            wind_morning = data_from_table_td[5].text
-            temp_evening = data_from_table_td[6].text
-            pres_evening = data_from_table_td[7].text
-            wind_evening = data_from_table_td[10].text
-
-            data_from_table.append(
-                {
-                    "data": data + "." + str(mouths) + "." + str(year),
-                    "temp_morning": temp_morning,
-                    "presure_morning": pres_morning,
-                    "wind_morning": wind_morning,
-                    "temp_evening": temp_evening,
-                    "presure_evening": pres_evening,
-                    "wind_evening": wind_evening
-                }
-            )
-
-        with open('dataset.csv', 'a', newline='') as csvfile:
-            for item in data_from_table:
-                file_writer = csv.writer(csvfile, delimiter=",", lineterminator="\r")
-                file_writer.writerow([item["data"], item["temp_morning"], item["presure_morning"], item["temp_evening"], item["presure_evening"], item["wind_evening"]])
