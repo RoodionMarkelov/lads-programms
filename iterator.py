f84f5996d20e6c5096f60ee07694f24bde6354a4
rodio <rodionmarkelov01229@gmail.com>
2023-12-03 22:27:02 +0400

    final 3 2.0

--- iterator.py
+++ iterator.py
@@ -1,25 +1,34 @@
 import csv
 
+
 class Iterator:
-    def __init__(self, csv_file:str):
+    def __init__(self, csv_file: str):
         self.csv_file = csv_file
         self._iter = self.generator()
 
     def generator(self):
-        with open(self.csv_file, newline ="") as f:
-            fieldnames = ['data', 'temp_morning', 'presure_morning', 'wind_morning', 'temp_evening', 'presure_evening',
-                          'wind_evening']
+        """Generate the iterator"""
+        with open(self.csv_file, newline="") as f:
+            fieldnames = [
+                "data",
+                "temp_morning",
+                "presure_morning",
+                "wind_morning",
+                "temp_evening",
+                "presure_evening",
+                "wind_evening",
+            ]
             reader = csv.DictReader(f, fieldnames=fieldnames)
             for row in reader:
-                date = row['data']
+                date = row["data"]
                 data = {
                     "data": date,
-                    "temp_morning": row['temp_morning'],
-                    "presure_morning": row['presure_morning'],
-                    "wind_morning": row['wind_morning'],
-                    "temp_evening": row['temp_evening'],
-                    "presure_evening": row['presure_evening'],
-                    "wind_evening": row['wind_evening']
+                    "temp_morning": row["temp_morning"],
+                    "presure_morning": row["presure_morning"],
+                    "wind_morning": row["wind_morning"],
+                    "temp_evening": row["temp_evening"],
+                    "presure_evening": row["presure_evening"],
+                    "wind_evening": row["wind_evening"],
                 }
                 yield data
 
@@ -27,13 +36,15 @@ class Iterator:
         return self
 
     def __next__(self):
+        """Get the next date in file"""
         try:
             return next(self._iter)
         except StopIteration:
             raise StopIteration
 
-if __name__ == '__main__':
-    csv_file = 'dataset.csv'
+
+if __name__ == "__main__":
+    csv_file = "dataset.csv"
     _iter = Iterator(csv_file)
     next_data = _iter.__next__()
     print(next_data)
@@ -43,5 +54,3 @@ if __name__ == '__main__':
     print(next_data)
     next_data = _iter.__next__()
     print(next_data)
-
-
--- weeks.py
+++ weeks.py
@@ -1,24 +1,50 @@
 import pandas as pd
 
-def group_by_week(csv_file:str):
-    '''The data from the file is grouped by week. Return list'''
-    df = pd.read_csv(csv_file, names=['date', 'temp_morning', 'presure_morning', 'wind_morning', 'temp_evening', 'presure_evening', 'wind_evening'], dtype=str)
-    df['date'] = pd.to_datetime(df['date'])
-    weekly_data = []
-    for name, group in df.set_index('date').groupby(pd.Grouper(freq='W')):
-        weekly_data.append(group.apply(lambda x: [str(x.name)[:10], str(x['temp_morning']), str(x['presure_morning']), str(x['wind_morning']), str(x['temp_evening']), str(x['presure_evening']), str(x['wind_evening'])], axis=1).tolist())
-    return weekly_data
 
-def divide_by_weeks(weeks_data:list) -> None:
-        '''Divide list by weeks and create the csv_files. Return None.'''
-        for week in weeks_data:
-            if week:
-                with open(f'week/{week[0][0].replace("-", "")}_{week[-1][0].replace("-", "")}.csv', 'w') as f:
-                    for day in week:
-                        f.write(f'{day[0]},{day[1]},{day[2]},{day[3]},{day[4]},{day[5]},{day[6]}\n')
+def divide_by_weeks(csv_file: str):
+    """Divide list by weeks and create the csv_files. Return None."""
+    df = pd.read_csv(
+        csv_file,
+        names=[
+            "date",
+            "temp_morning",
+            "presure_morning",
+            "wind_morning",
+            "temp_evening",
+            "presure_evening",
+            "wind_evening",
+        ],
+        dtype=str,
+    )
+    df["date"] = pd.to_datetime(df["date"])
+    weeks_data = []
+    for name, group in df.set_index("date").groupby(pd.Grouper(freq="W")):
+        weeks_data.append(
+            group.apply(
+                lambda x: [
+                    str(x.name)[:10],
+                    str(x["temp_morning"]),
+                    str(x["presure_morning"]),
+                    str(x["wind_morning"]),
+                    str(x["temp_evening"]),
+                    str(x["presure_evening"]),
+                    str(x["wind_evening"]),
+                ],
+                axis=1,
+            ).tolist()
+        )
+    for week in weeks_data:
+        if week:
+            with open(
+                f'week/{week[0][0].replace("-", "")}_{week[-1][0].replace("-", "")}.csv',
+                "w",
+            ) as f:
+                for day in week:
+                    f.write(
+                        f"{day[0]},{day[1]},{day[2]},{day[3]},{day[4]},{day[5]},{day[6]}\n"
+                    )
+    return
 
 
 if __name__ == "__main__":
-    data = group_by_week('dataset.csv')
-    divide_by_weeks(data)
-
+    divide_by_weeks("dataset.csv")
